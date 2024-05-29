de08e16c82290f6cef3cf0a59f1839ccf027e168
NgoHoangMinh2002 <minhngo1409@gmail.com>
2024-05-29 11:09:24 +0700

    ls06

---
+++ Controllers/NhmSongController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NhmLesson06.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace NhmLesson06.Controllers
+{
+    public class NhmSongController : Controller
+    {
+        private static List<NhmSong> nhmSongs = new List<NhmSong>()
+        {
+            new NhmSong{Id=22109044,NhmTitle="NgoHoangMinh", NhmArtist="CNTT",NhmAuthor="NTU", NhmYearRelease=2023},
+            new NhmSong{Id=1,NhmTitle="NgoHoangMinh", NhmArtist="K22CNTT",NhmAuthor="NTU", NhmYearRelease=2020}
+        };
+    
+        
+        // GET: NhmSong
+        public ActionResult NhmIndex()
+        {
+            return View(nhmSongs);
+        }
+        public ActionResult NhmCreate()
+        {
+            var nhmSong = new NhmSong();
+            return View(nhmSong);
+        }
+    }
+}
---
+++ Models/NhmSong.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NhmLesson06.Models
+{
+    public class NhmSong
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Nhm: Hãy nhâp ứng viên")]
+        [DisplayName("Tieu De")]
+        public string NhmTitle { get; set; }
+        [Required(ErrorMessage = "Nhm: Hay Nhap nhan vien")]
+        [DisplayName("NV chinh")]
+        public string NhmAuthor { get; set; }
+        [Required(ErrorMessage = "Nhm: Hay Nhap ten")]
+        [StringLength (50,MinimumLength =2,ErrorMessage ="Ten co toi thieu 2 ky tu,toi da 50 ky tu")]
+        public string NhmArtist { get; set; }
+        [Required(ErrorMessage = "Nhm: Hay Nhap Nguoi Dung")]
+        [RegularExpression(@"[0-9][4]",ErrorMessage ="Nhm:Nhap xuat phai co 2 ky tu so")]
+        [Range(1900,2004,ErrorMessage ="Nhm:Nam sinh trong khoang tu 1900-2004")]
+        [DisplayName("Nam Sinh")]
+        public int NhmYearRelease { get; set; }
+
+    }
+}
